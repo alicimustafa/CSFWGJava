6a131ef62a8ea0913c0e9f9d4cd6ef4c427b780e
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2018-01-31 12:04:34 -0700

    unit test for login done

--- csfwgREST/test/controllers/AuthControllersTest.java
+++ csfwgREST/test/controllers/AuthControllersTest.java
@@ -2,60 +2,63 @@ package controllers;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.After;
-import org.junit.Before;
+import java.io.IOException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import builders.LoginBuilder;
+import builders.MemberBuilder;
+import builders.RankBuilder;
+import data.MemberDAO;
 import entities.Login;
+import entities.Member;
+import entities.Rank;
 
 @RunWith(SpringJUnit4ClassRunner.class) 
 @ContextConfiguration(locations = { "AuthControllersTest-context.xml" })  
 @WebAppConfiguration  
 public class AuthControllersTest {
 
-	MockMvc mockMvc;
+	private MockMvc mockMvc;
 
 	@Autowired
-	WebApplicationContext wc;
+	private WebApplicationContext wc;
 	
 	@Autowired
-	AuthController ac;
-	
+	private MemberDAO dao;
+	 
 	@Autowired
-	MockMemberDAO dao;
-	
-	@Before
-	public void setUp() {
-		dao = wc.getBean(MockMemberDAO.class); 
-		dao.init();
-		ac.setDao(dao);
-		mockMvc = MockMvcBuilders.webAppContextSetup(wc).build(); 
-	}
-
-
-	@After
-	public void tearDown() throws Exception {
-		dao.init();
-	}
+	private AuthController ac;
 	
 	@Test
-	public void login_returns_correct_json() {
-		Login log = new Login();
-		log.setUsername("jody@gmail.com");
-		log.setPassword("1234");
+	public void login_returns_correct_json_if_correct_login() {
+		Login log = new LoginBuilder().username("jody@gmail.com").password("1234").build();
+		Rank rank = new RankBuilder().id(1).name("Admin").build();
+		Member member = new MemberBuilder()
+				.firstName("Jody")
+				.lastName("Morgan")
+				.id(1)
+				.rank(rank)
+				.build();
+		
+		when(dao.loginMember("")).thenReturn(member);
 		try {
 			mockMvc.perform(post("auth/login")
 					.contentType(TestUtil.APPLICATION_JSON_UTF8)
@@ -69,8 +72,26 @@ public class AuthControllersTest {
 	                .andExpect(jsonPath("$[0].lastName", is("Morgan")))
 	                .andExpect(jsonPath("$[0].rank.id", is(1)))
 	                .andExpect(jsonPath("$[1].rank.name", is("Admin")));
-	 
+			verify(dao, times(1)).loginMember("");
+	        verifyNoMoreInteractions(dao);
 			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void login_returns_405_if_login_info_incorrect_password() {
+		Login log = new LoginBuilder().username("jody@gmail.com").password("2234").build();
+		when(dao.loginMember("")).thenReturn(null);
+		try {
+			mockMvc.perform(post("auth/login")
+					.contentType(TestUtil.APPLICATION_JSON_UTF8)
+					.content(TestUtil.convertObjectToJsonBytes(log)))
+					.andExpect(status().is4xxClientError());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
